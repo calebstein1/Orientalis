eff111ff1858a2164e1c5cca9053e0440ea07273
Caleb Stein <caleb@calebstein.net>
2024-07-31 23:54:04 -0700

    cool intro scene and fix music fade bug

--- music.lua
+++ music.lua
@@ -1,5 +1,5 @@
 function play_music_for_location()
-    if not music_playing then
+    if not music_playing and frame-overworld_timer>=15 then
         music_playing=true
         local m
 
@@ -8,9 +8,9 @@ function play_music_for_location()
         elseif p_map==1 then
             m=-1
         elseif p_map==2 then
-            m=-1
-        elseif p_map==3 then
-            m=-1
+            m=2
+        elseif p_map==3 or p_map==6 then
+            m=4
         elseif p_map==4 then
             if p_state==7 then
                 m=-1
@@ -19,21 +19,24 @@ function play_music_for_location()
             end
         elseif p_map==5 then
             m=-1
-        elseif p_map==6 then
-            m=-1
         elseif p_map==7 then
-            m=-1
+            if not event_flags[3] then
+                m=0
+            else
+                m=-1
+            end
         elseif p_map==8 then
             m=-1
         elseif p_map==9 then
             m=-1
         end
 
-        music(m,300)
+        music(m,500)
     end
 end
 
 function stop_music()
+    overworld_timer=frame
     music_playing=false
-    music(-1,300)
+    music(-1,500)
 end
