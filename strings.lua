8a0a3fc87deacf21eee198628dada97b2a582de5
Caleb Stein <caleb@calebstein.net>
2024-08-01 00:48:20 -0700

    add number comments

--- strings.lua
+++ strings.lua
@@ -1,36 +1,52 @@
 function load_strings()
     strings={
+        -- 1
         {"hi there, "..p_name..", what can i do","for you today?"},
 
+        -- 2
         {"earthquake woke you too, huh?","sure sounded like it came","from back in the cave.","i'd go check it out myself","but... well you know..."},
 
+        -- 3
         {"hi "..p_name.."!","beautiful day, isn't it?","so listen, hank, the old","fisherman, sounded like he'd","been drinking again.",
         "might be a good idea to steer","clear until he sobers up"},
 
+        -- 4
         {"get out *hic* of here!","i'm in no mood for guests"},
 
+        -- 5
         {"if you're planning on heading","into the woods south of town,","make sure you're prepared for","the thick fog you'll find","there.","can't hardly see a thing"},
 
+        -- 6
         {"those earthquakes are causing","some molten rock to rise up","to the surface.",
         "be careful if you're heading","down into the cave, it's","pretty dark, and a step on","those molten rocks will","sure hurt"},
 
+        -- 7
         {"hi big sister!","i heard you get to go on","a big adventure to save","the world! that's so cool!",
         "take these first aid kits","i have, i hope they help!"},
 
+        -- 8
         {"i wanna go on an adventure","like you someday"},
 
+        -- 9
         {p_name.."...","i'm glad you came...","i called you here for a","specific reason...","i am from the future","in my time, demonic forces","have overrun the world...",
         "i believe you are the","chosen one to defeat the","evil and save us all...",
         "my pow... to communic...","is fading... go to the","...ave in the east firs...",
         "your path wi.. contain man...","trials... keep fait... and","goo... luck"},
 
+        -- 10
         {"normally i'd tell you to go","back to bed and *not* to","sneak out and explore what","caused that earthquake,","but you are my daughter,","and i know you'll sneak out","no matter what i say, so",
         "instead i'll just tell you","to please be careful.","oh, and maybe let your little","brother know you're going","out if you haven't already"},
 
+        -- 11
         {"welcome home, "..p_name..",","i know it's nearly daybreak,","but you've had a long night,","you might want to head to","your room and have a quick","nap before you head off again"},
 
+        -- 12
         {"you look a bit tired, honey","your bed's always made up","for you in case you need it"},
 
-        {"this place is mine","you cannot take it from me","you're on an adventure to","save the world?","haw haw haw...","i shall enjoy snacking on you"}
+        -- 13
+        {"this place is mine","you cannot take it from me","you're on an adventure to","save the world?","haw haw haw...","i shall enjoy snacking on you"},
+
+        -- 14
+        {"oh honey, you look so sad!","come here, now that's better,","isn't it?","we're all so proud of you but","remember, you can always come","home if you need, that's what","family is for"}
     }
 end
